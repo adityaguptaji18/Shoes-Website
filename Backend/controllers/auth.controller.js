30a01cf5e07f2837636893ef1c741239a7c298d0
adityaguptaji18 <ag2500310@gmail.com>
2026-04-15 15:06:10 +0530

    Day 2, 3, 4 - User Model, Auth APIs, JWT Middleware and Protected Routes

---
+++ Backend/controllers/auth.controller.js
@@ -0,0 +1,59 @@
+import UserModel from "../models/user.model.js";
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+async function registerUser(req,res) {
+  
+  try {
+    const {name,email,password}=req.body;
+    let existingUser=await UserModel.findOne({email})
+    if(existingUser){
+    return res.status(400).json({
+      message:"User Already Exists"
+    })
+  }
+  const hashedPassword=await bcrypt.hash(password,10);
+  const user=await UserModel.create({name,email,password:hashedPassword});
+  if(user){
+    return res.status(201).json({
+      message:"User registered successfully"
+    })
+  }
+  } catch (error) {
+    res.status(500).json({
+      message:"Internal Server Error in RegisterUser"
+    })
+  }
+}
+
+async function loginUser(req,res) {
+  try {
+    const{email,password}=req.body;
+    let existingUser=await UserModel.findOne({email})
+    if(!existingUser){
+      return res.status(401).json({
+        message:"User Not Exists"
+    })
+  }
+  const isMatch=await bcrypt.compare(password,existingUser.password)
+  if(!isMatch) {
+    return res.status(401).json({message:"invalid Password"})
+  }
+  const token=jwt.sign({
+    id:existingUser._id,
+    role:existingUser.role
+  },process.env.JWT_SECRET,
+  {expiresIn:"7d"}
+)
+  return res.status(200).json({
+    message:"Login Successful",
+    token:token
+  })
+  } catch (error) {
+    return res.status(500).json({
+      message:"Internal Server Error in LoginUser"
+    })
+  }
+  
+}
+
+export  {registerUser,loginUser}
--- Backend/index.js
+++ Backend/index.js
@@ -2,12 +2,22 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/auth.routes.js"
+import verifyToken from "./middleware/auth.middleware.js";
 const app=express();
 app.use(express.json())
 connectDB();
 app.get("/",(req,res)=>{
   res.send("Server is running")
 })
+app.use("/api/auth",authRouter);
+app.get("/api/test",verifyToken,(req,res)=>{
+  res.json({
+    message:"Access Granted",
+    user:req.user
+  })
+})
+
 app.listen(process.env.PORT,()=>{
   console.log("app is running at port "+process.env.PORT)
 } )
