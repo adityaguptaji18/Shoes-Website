6e565a82397b36b3fd2aac741b5f2977ca08630e
adityaguptaji18 <ag2500310@gmail.com>
2026-04-21 01:57:04 +0530

    Day 6 - getProductById, updateProduct, deleteProduct and filtering

--- Backend/controllers/product.controller.js
+++ Backend/controllers/product.controller.js
@@ -1,13 +1,23 @@
 import productModel from "../models/product.model.js";
-
+import mongoose from "mongoose";
 async function getAllProducts(req,res) {
   try {
-   const products= await productModel.find()
+    const {category , minPrice , maxPrice , size}=req.query;
+    const filter={}
+    if(category) filter.category=category;
+    if(size) filter.sizes=size
+    if(minPrice || maxPrice){
+      filter.price={}
+      if(minPrice) filter.price.$gte=Number(minPrice)
+      if(maxPrice) filter.price.$lte=Number(maxPrice)
+    }
+   const products= await productModel.find(filter)
     return res.status(200).json({
       products,
       message:"These are all products"
     })
   } catch (error) {
+    console.log(error);
     return res.status(500).json({
       message:"there is an error in getAllProducts"
     })
@@ -22,10 +32,74 @@ async function createProduct(req,res) {
       message:"wuhuuuu product successfully created"
     })
   } catch (error) {
+    console.log(error);
     return res.status(500).json({
       message:"there is an error in creating product"
     })
   }
 }
 
-export {getAllProducts,createProduct}
+async function getproductById(req,res){
+  const id=req.params.id;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+  return res.status(400).json({ message: "Invalid Product ID" });
+}
+  try {
+    const product=await productModel.findById(id);
+    if(!product){
+      return res.status(404).json({message:"Product not found"});
+    }
+    return res.status(200).json({
+      product,
+      message:"this product is what you are search for"
+    })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message:"Internal Server error"
+    })
+  }
+}
+
+async function updateProduct(req,res){
+  const id=req.params.id;
+  const update=req.body;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+  return res.status(400).json({ message: "Invalid Product ID" });
+}
+  try {
+    const product=await productModel.findByIdAndUpdate(id,update,{new:true});
+    if(!product){
+      return res.status(404).json({message:"Product not found"})
+    }
+    return res.status(200).json({
+      product,message:"Product changes successfully wuhuu"
+    })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({message:"Internal Server Error"})
+  }
+}
+
+async function deleteProduct(req,res) {
+  const id=req.params.id;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+  return res.status(400).json({ message: "Invalid Product ID" });
+}
+  try {
+    const product=await productModel.findByIdAndDelete(id);
+    if(!product){
+      return res.status(404).json({message:"Product not found"});
+    }
+    return res.status(200).json({product,
+      message:"this product is deleted succesfully"
+    })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message:"Internal Server Error"
+    })
+  }
+}
+
+export {getAllProducts,createProduct,getproductById,updateProduct,deleteProduct}
--- Backend/routes/product.routes.js
+++ Backend/routes/product.routes.js
@@ -1,8 +1,11 @@
-import { getAllProducts,createProduct } from "../controllers/product.controller.js";
+import { getAllProducts,createProduct , getproductById ,updateProduct,deleteProduct} from "../controllers/product.controller.js";
 import express from "express";
 import {verifyToken,isAdmin} from "../middleware/auth.middleware.js";
 const router=express.Router()
 
 router.get("/",getAllProducts);
+router.get("/:id",getproductById)
 router.post("/",verifyToken,isAdmin,createProduct)
+router.put("/:id",verifyToken,isAdmin,updateProduct)
+router.delete("/:id",verifyToken,isAdmin,deleteProduct)
 export default router
