cf8dca895e3e76bf8f16ddb9b25438c66a68068a
adityaguptaji18 <ag2500310@gmail.com>
2026-06-07 03:35:48 +0530

    Centre Alignment Issue fix + Back to Products Button

--- Frontend/src/pages/ProductDetail.jsx
+++ Frontend/src/pages/ProductDetail.jsx
@@ -71,7 +71,8 @@ import '../styles/ProductDetail.css';
 
       {product&& (
         <>
-
+        <button
+          className="back-btn" onClick={() => navigate('/products')}>Back to Products </button>
           <img src={product.images?.[0]|| 'https://placehold.co/400x400?text=No+Image'} alt={product.name} />
           <div className="product-info">
           <h2>{product.name}</h2>
